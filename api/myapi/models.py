72cf4a19e0812a7be8a6e764c3a958865133ec52
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-11-11 14:11:46 +0100

    create User class and add attributs

--- api/myapi/models.py
+++ api/myapi/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    age=models.IntegerField()
     name=models.CharField(max_length=10)
+    age=models.IntegerField()
     
     def __str__(self):
         return self.name
