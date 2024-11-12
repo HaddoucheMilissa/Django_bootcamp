7ff271e2756820d6a7f5bf46e50619c3cafc56ef
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-11-13 00:10:33 +0100

    using api view get,put,delete at the same time

--- api/myapi/urls.py
+++ api/myapi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_user, create_user
+from .views import get_user, create_user, user_requests
 
 urlpatterns = [
     path('user/', get_user, name='get_user_data'),
-    path('user/create', create_user, name='post_user_data')
+    path('user/create', create_user, name='post_user_data'),
+    path('user/<int:pk>', user_requests,name="pk")
 ]
